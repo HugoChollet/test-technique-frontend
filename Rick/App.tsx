61fdfbd86c2631e14c50c090c7b705e48fca9975
HugoChollet <tibolescabo@gmail.com>
2023-06-20 16:18:40 +0200

    add basic fetching for character API

--- Rick/App.tsx
+++ Rick/App.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {SafeAreaView, useColorScheme} from 'react-native';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 import CharacterList from './src/Components/CharacterList/CharacterList';
+import {getData} from './src/Components/getData';
+import {Character} from './src/types/Character';
+
+const apiGetCharacter = 'https://rickandmortyapi.com/api/character';
 
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
+  const [characters, setCharacters] = useState<Character[]>();
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
@@ -16,13 +21,39 @@ function App(): JSX.Element {
       'https://www.radiofrance.fr/s3/cruiser-production/2019/10/22f8d83b-2dbb-4156-8f6d-9cc13b94e16f/1200x680_rickmorty.jpg',
     name: 'test',
     status: 'test',
-    race: 'test',
-    firstSeenEpisode: 'test',
+    species: 'test',
+    location: 'test',
   };
+
+  useEffect(() => {
+    getData(apiGetCharacter).then(data => {
+      console.log('all fetched data', data);
+
+      setCharacters(
+        data.results.map(char => {
+          console.log('test');
+
+          console.log('ici', char);
+
+          return {
+            name: char.name,
+            image: char.image,
+            status: char.status,
+            species: char.species,
+            location: char.location.name,
+          };
+        }),
+      );
+    });
+  }, []);
+
+  useEffect(() => {
+    console.log(characters);
+  }, [characters]);
 
   return (
     <SafeAreaView style={backgroundStyle}>
-      <CharacterList characters={[characterMocked, characterMocked]} />
+      <CharacterList characters={characters || [characterMocked]} />
     </SafeAreaView>
   );
 }
