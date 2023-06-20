39829273f05385648fcbdb7c567bb084a6a5419e
HugoChollet <tibolescabo@gmail.com>
2023-06-20 17:07:57 +0200

    add fetch refresh on scroll

--- Rick/App.tsx
+++ Rick/App.tsx
@@ -6,35 +6,32 @@ import CharacterList from './src/Components/CharacterList/CharacterList';
 import {getData} from './src/Components/getData';
 import {Character} from './src/types/Character';
 
-const apiGetCharacter = 'https://rickandmortyapi.com/api/character';
+const characterMocked = {
+  image:
+    'https://www.radiofrance.fr/s3/cruiser-production/2019/10/22f8d83b-2dbb-4156-8f6d-9cc13b94e16f/1200x680_rickmorty.jpg',
+  name: 'Rick&Morty',
+  status: 'Okay',
+  species: 'Cartoons Characters',
+  location: 'TV',
+};
 
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
   const [characters, setCharacters] = useState<Character[]>();
+  const [apiGetCharacter, setApiGetCharacter] = useState(
+    'https://rickandmortyapi.com/api/character',
+  );
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
-  const characterMocked = {
-    image:
-      'https://www.radiofrance.fr/s3/cruiser-production/2019/10/22f8d83b-2dbb-4156-8f6d-9cc13b94e16f/1200x680_rickmorty.jpg',
-    name: 'test',
-    status: 'test',
-    species: 'test',
-    location: 'test',
-  };
-
-  useEffect(() => {
+  const fetchCharacters = () => {
     getData(apiGetCharacter).then(data => {
-      console.log('all fetched data', data);
-
-      setCharacters(
-        data.results.map(char => {
-          console.log('test');
-
-          console.log('ici', char);
-
+      setApiGetCharacter(data.info.next);
+      setCharacters([
+        ...(characters || []),
+        ...data.results.map(char => {
           return {
             name: char.name,
             image: char.image,
@@ -43,17 +40,22 @@ function App(): JSX.Element {
             location: char.location.name,
           };
         }),
-      );
+      ]);
     });
-  }, []);
+  };
 
   useEffect(() => {
-    console.log(characters);
-  }, [characters]);
+    fetchCharacters();
+  }, []);
 
   return (
     <SafeAreaView style={backgroundStyle}>
-      <CharacterList characters={characters || [characterMocked]} />
+      {characters ? (
+        <CharacterList
+          characters={characters || [characterMocked]}
+          onEndReached={() => fetchCharacters()}
+        />
+      ) : null}
     </SafeAreaView>
   );
 }
--- Rick/src/Components/CharacterList/CharacterList.tsx
+++ Rick/src/Components/CharacterList/CharacterList.tsx
@@ -5,15 +5,20 @@ import CharacterCard from '../CharacterCard/CharacterCard';
 
 type CharacterListProps = {
   characters: Character[];
+  onEndReached: () => void;
 };
 
-const CharacterList: React.FC<CharacterListProps> = ({characters}) => {
+const CharacterList: React.FC<CharacterListProps> = ({
+  characters,
+  onEndReached,
+}) => {
   return (
     <View style={styles.container}>
       <FlatList
         data={characters}
         renderItem={({item}) => <CharacterCard character={item} />}
         keyExtractor={(item, index) => index.toString()}
+        onEndReached={onEndReached}
       />
     </View>
   );
